16a572057f0b64ecad883b5d64cb29545862e1b0
_vision <shefroy.rowe@gmail.com>
2024-10-06 11:12:30 -0400

    comment script.js code

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const containerSize = 32;
-const gridSize = prompt("Enter grid size",);
+const containerSize = 32; //32em css size
+const gridSize = prompt("Enter grid size",);//limit to 100x100 grid and convert to input element
 
 const container = document.getElementById('grid-container');
 container.style.width = `${containerSize}em`;
 container.style.height = `${containerSize}em`;
 
-
+//function to dynamically generate grids of any size
 const makeGrid = () => {
-    const fullGridSize = (gridSize * gridSize);
-    const gridBoxSize = `${(containerSize / gridSize) - 0.125}em`;
+    const fullGridSize = (gridSize * gridSize);//complete number of grid cells
+    const gridBoxSize = `${(containerSize / gridSize) - 0.125}em`;//0.125 equivalent to 2px
 
     for (let i = 0; i < fullGridSize; i++) {
         const gridBox = document.createElement('div');
+
+        //grid cell height and width assignment
         gridBox.style.width = gridBoxSize;
         gridBox.style.height = gridBoxSize;
 
-        gridBox.classList.add('grid-box');
+        //add class to each cell and append to contaner
+        gridBox.className = 'grid-box';
         container.appendChild(gridBox);
-    }
+    }//end for loop
 }
 
+//call makeGrid
 makeGrid();
