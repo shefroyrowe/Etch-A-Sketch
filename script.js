72a1b84900e7f7e03daf9ab10cbab3b55aaa5a55
_vision <shefroy.rowe@gmail.com>
2024-10-06 10:29:58 -0400

    refactor stylesheet, add function to create div grids dynamically

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const container = document.getElementById('grid-container');
-const gridBox = document.createElement('div');
-gridBox.className = "grid-box";
+const containerSize = 32;
+const gridSize = prompt("Enter grid size",);
 
+const container = document.getElementById('grid-container');
+container.style.width = `${containerSize}em`;
+container.style.height = `${containerSize}em`;
+
+
+const makeGrid = () => {
+    const fullGridSize = (gridSize * gridSize);
+    const gridBoxSize = `${(containerSize / gridSize) - 0.125}em`;
+
+    for (let i = 0; i < fullGridSize; i++) {
+        const gridBox = document.createElement('div');
+        gridBox.style.width = gridBoxSize;
+        gridBox.style.height = gridBoxSize;
+
+        gridBox.classList.add('grid-box');
+        container.appendChild(gridBox);
+    }
+}
+
+makeGrid();
