9f7164feba890dd0f74bcf101052964909bf737d
_vision <shefroy.rowe@gmail.com>
2024-10-06 21:34:59 -0400

    add functions and switch to generate and toggle between black-randomized-shading and eraser features respectively

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const containerSize = 32; //32em css unit size
-const gridSize = prompt("Enter grid size between 1 - 100",);//limit to 100x100 grid and convert to input element
+//const gridSize = prompt("Enter grid size between 1 - 100",);//limit to 100x100 grid and convert to input element
 
 //query and stle container with width and height
 const container = document.getElementById('grid-container');
@@ -8,6 +8,22 @@ container.style.height = `${containerSize}em`;//add unit size(em)
 
 //get feature buttons
 const featureButtons = document.querySelectorAll('button');
+
+//random color generator 
+const getRandomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 17)];
+    }
+    return color;
+  }
+
+  //fade to black generator
+  const fadeIn = (element) => {
+    let random = Math.floor(Math.random() * 255);
+     element.style.backgroundColor = `rgb(${random},${random},${random})`;
+  }
 
 //function to dynamically generate grids of any size
 const makeGrid = () => {
@@ -25,9 +41,34 @@ const makeGrid = () => {
         gridBox.className = 'grid-box';
         container.appendChild(gridBox);
 
-        gridBox.addEventListener('mouseover', () => {
-            gridBox.style.backgroundColor = "black";
+        //color features switch statement
+        featureButtons.forEach((button) => {
+            button.addEventListener("click", (e) => {
+                switch (e.target.id) {
+                    case "black":
+                        gridBox.addEventListener('mouseover', () => {
+                            gridBox.style.backgroundColor = "black";
+                        });
+                        break;
+                    case "rainbow":
+                        gridBox.addEventListener('mouseover', () => {
+                            gridBox.style.backgroundColor = getRandomColor();
+                        });
+                        break;
+                        case "fade-in":
+                        gridBox.addEventListener('mouseover', () => { 
+                            fadeIn(gridBox);
+                        });
+                        break;
+                        case "erase":
+                        gridBox.addEventListener('mouseover', () => { 
+                            gridBox.style.backgroundColor = '';
+                        });
+                        break;
+                }
+            });
         });
+        
     }//end for loop
 }
 
